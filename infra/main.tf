42a69f19f71434d6cf3730a07aa204fb380d5a87
Chayanne_Sanchez <sanchez_ocana@hotmail.com>
2026-04-19 22:54:18 -0500

    feat(init): setup static site with terraform and github actions

---
+++ infra/main.tf
@@ -0,0 +1,56 @@
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket  = "chayanne-terraform-state"
+    key     = "github-actions-demo/terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
+  }
+}
+
+resource "aws_s3_bucket" "web" {
+  bucket = "website-chayanne-github-actions"
+}
+
+resource "aws_s3_bucket_website_configuration" "web_config" {
+  bucket = aws_s3_bucket.web.id
+
+  index_document {
+    suffix = "index.html"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "web_public" {
+  bucket = aws_s3_bucket.web.id
+
+  block_public_acls   = false
+  block_public_policy = false
+  ignore_public_acls  = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "public_read" {
+  bucket = aws_s3_bucket.web.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = "*",
+      Action = "s3:GetObject",
+      Resource = "arn:aws:s3:::website-chayanne-github-actions/*"
+    }]
+  })
+}
+
+output "bucket_name" {
+  value = aws_s3_bucket.web.bucket
+}
+
+output "website_url" {
+  value = "http://${aws_s3_bucket.web.bucket}.s3-website-us-east-1.amazonaws.com"
+}
